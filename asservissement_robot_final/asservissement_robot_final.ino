03a32dc21f0b1d79e4177b8146d59acaa3ce32ab
JonathanPlasse <jonathan.plasse@live.fr>
2018-11-18 23:31:53 +0100

    Asservissement en position fonctionnel

--- asservissement_robot_final/asservissement_robot_final.ino
+++ asservissement_robot_final/asservissement_robot_final.ino
@@ -13,35 +13,35 @@ double positionGauche = 0, positionDroite = 0;
 double dernierepositionGauche = 0, dernierepositionDroite = 0;
 
 /*Moteur*/
-#define N_FILTRE 3
+#define N_FILTRE 4
 unsigned long dernierTemps, maintenant, deltaTemps; //en ms
 double vitesseGauche, vitesseDroite, vitessesGauche[N_FILTRE] = {0}, vitessesDroite[N_FILTRE] = {0};//en tours/seconde
 double vitesseLineaire, vitesseRotation;
 int iGauche = 0, iDroite = 0;
 const double tour = 1633;//408.25 pas par tour
 double pwmMax = 255;
-double vitesseMax = 3;
+double vitesseMax = 2;
 MCC moteurGauche(A0, A1, 9), moteurDroite(A2, A3, 10);
 
 /*Odométrie*/
 double x = 0, y = 0, theta = 0, distanceLineaire, distanceRotation;
-double consigneX = 1, consigneY = 1, consigneTheta = 0;
-double consignePosLineaire = sqrt(sq(x - consigneX) + sq(y - consigneY)), consignePosRotation = atan2(consigneY - y, consigneX - x) - theta;
-double positionLineaire, positionRotation;
+double consigneX = 100, consigneY = 0, consigneTheta = 0;
+double consignePosLineaire, consignePosRotation;
+double positionLineaire = 0, positionRotation = 0;
 
 /*PID*/
-double consigneVitLineaire = 2, consigneVitRotation = 0;
+double consigneVitLineaire = 0, consigneVitRotation = 0;
 double commandeVitLineaire, commandeVitRotation; //la commande est le pwm envoyé sur le moteur
 unsigned int echantillonnage = 2; //l'échantillonnage est l'intervalle de temps entre chaque calcul de la commande, exprimé en milliseconde
 
 //Réglage des coefficient des PID
-const double kpVit = 2200;
-const double kiVit = 0;
+const double kpVit = 2000;
+const double kiVit = 1000;
 const double kdVit = 0;
 
-const double kpPos = 0;
+const double kpPos = 1;
 const double kiPos = 0;
-const double kdPos = 0;
+const double kdPos = 0.01;
 
 PID positionLineairePID(&positionLineaire, &consigneVitLineaire, &consignePosLineaire, kpPos, kiPos, kdPos, DIRECT);
 PID positionRotationPID(&positionRotation, &consigneVitRotation, &consignePosRotation, kpPos, kiPos, kdPos, DIRECT);
@@ -72,14 +72,17 @@ void setup() {
   /*Initialisation de la liaison série*/
   Serial.begin(115200);
 
+  consignePosLineaire = sqrt(sq(x - consigneX) + sq(y - consigneY)) / COEFF_D / 1000 * tour + positionLineaire;
+  consignePosRotation = (atan2(consigneY - y, consigneX - x) - theta) / COEFF_R / 1000 * tour + positionRotation;
+
   //Initialisation PID
   positionLineairePID.SetSampleTime(echantillonnage);
   positionLineairePID.SetOutputLimits(-vitesseMax, vitesseMax);
-  //positionLineairePID.SetMode(AUTOMATIC);
+  positionLineairePID.SetMode(AUTOMATIC);
 
   positionRotationPID.SetSampleTime(echantillonnage);
   positionRotationPID.SetOutputLimits(-vitesseMax, vitesseMax);
-  //positionRotationPID.SetMode(AUTOMATIC);
+  positionRotationPID.SetMode(AUTOMATIC);
 
   vitesseLineairePID.SetSampleTime(echantillonnage);
   vitesseLineairePID.SetOutputLimits(-pwmMax, pwmMax);
@@ -121,13 +124,15 @@ void loop() {
 
     dernierTemps = maintenant;
   }
+  positionLineairePID.Compute();
+  positionRotationPID.Compute();
   vitesseLineairePID.Compute();
   vitesseRotationPID.Compute();
   moteurGauche.bouger((int)commandeVitLineaire - commandeVitRotation);
   moteurDroite.bouger((int)commandeVitLineaire + commandeVitRotation);
 
   //Affichage liaison série
-  Serial.print(vitesseLineaire);
-  Serial.print(" 0 2 ");
-  Serial.println(vitesseRotation);
+  Serial.print(positionLineaire);
+  Serial.print(" 0 ");
+  Serial.println(consignePosLineaire);
 }
